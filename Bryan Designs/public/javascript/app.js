f111aee8fdbd430fdbf164b51883ab07a3f66c68
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-12 12:36:17 -0700

    Added a folder for design. Includes the following: homepage(index.html), javascript, and css

---
+++ Bryan Designs/public/javascript/app.js
@@ -0,0 +1,11 @@
+//================================ [Menu Bar] ================================//
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
