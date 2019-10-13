78ec57a274913ee6e96094c6425917a92031d3a4
Alexei Dias <alexeidias@gmail.com>
2019-10-13 13:23:55 -0700

    api routes fixed

--- routesfinlazed/artist-api-routes.js
+++ routesfinlazed/artist-api-routes.js
@@ -7,8 +7,8 @@ module.exports = function(app) {
     // In this case, just db.Post
     db.Artist.findAll({
       include: [db.Artwork]
-    }).then(function(dbArtwork) {
-      res.json(dbArtwork);
+    }).then(function(dbArtist) {
+      res.json(dbArtist);
     });
   });
 
@@ -21,8 +21,8 @@ module.exports = function(app) {
         id: req.params.id
       },
       include: [db.Artwork]
-    }).then(function(dbArtwork) {
-      res.json(dbArtwork);
+    }).then(function(dbArtist) {
+      res.json(dbArtist);
     });
   });
 
