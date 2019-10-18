ea5a532f685ebce9b773abf8f252b77efe282f38
Jerry Dudum <jsdudum@gmail.com>
2019-10-17 18:01:43 -0700

    had to uncomment some api routes

--- routes/artist-api-routes.js
+++ routes/artist-api-routes.js
@@ -8,15 +8,15 @@ module.exports = function(app) {
     });
   });
 
-  // app.get("/api/artists/:id", function(req, res) {
-  //   db.Artist.findOne({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   }).then(function(dbArtist) {
-  //     res.json(dbArtist);
-  //   });
-  // });
+  app.get("/api/artists/:id", function(req, res) {
+    db.Artist.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbArtist) {
+      res.json(dbArtist);
+    });
+  });
 
   app.post("/api/artists", function(req, res) {
     db.Artist.create(req.body).then(function(dbArtist) {
--- routes/artwork-api-routes.js
+++ routes/artwork-api-routes.js
@@ -8,15 +8,15 @@ module.exports = function(app) {
     });
   });
 
-  // app.get("/api/artwork/:id", function(req, res) {
-  //   db.Artwork.findOne({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   }).then(function(dbArtwork) {
-  //     res.json(dbArtwork);
-  //   });
-  // });
+  app.get("/api/artwork/:id", function(req, res) {
+    db.Artwork.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbArtwork) {
+      res.json(dbArtwork);
+    });
+  });
 
   app.post("/api/artwork", function(req, res) {
     db.Artwork.create(req.body).then(function(dbArtwork) {
