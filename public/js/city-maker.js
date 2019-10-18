6664262f3bc3a2e54c59f315fd8db5c15cab5ae0
Jerry Dudum <jsdudum@gmail.com>
2019-10-17 20:20:16 -0700

    added a delete function to our 'hidden' manager route to delete unwanted artists and artworks

--- public/js/city-maker.js
+++ public/js/city-maker.js
@@ -21,4 +21,78 @@ $(document).ready(function () {
         });
 
     })
+
+    function getArtists() {
+
+        $("#artistDelete").empty();
+
+        $.get("/api/artists", function (data) {
+
+            console.log(data);
+
+            for (var i = 0; i < data.length; i++) {
+                var artistDiv = $("<div>");
+                artistDiv.addClass("col-12");
+                var deleteBtn = $("<button>");
+                artistDiv.append("<h4>" + data[i].name + "</h4>");
+                artistDiv.append(deleteBtn)
+                deleteBtn.addClass("deleteArtist btn btn-dark");
+                deleteBtn.text("DELETE");
+                deleteBtn.attr("data-id", data[i].id);
+                $("#artistDelete").append(artistDiv);
+            }
+
+
+        })
+    }
+    getArtists();
+
+    $(document).on("click", ".deleteArtist", function () {
+
+        var artistID = $(this).attr("data-id");
+
+        $.ajax({
+            method: "DELETE",
+            url: "/api/artists/" + artistID
+        }).then(getArtists());
+    })
+
+
+
+    function getArtworks() {
+
+        $("#artworkDelete").empty();
+
+        $.get("/api/artwork", function (data) {
+
+            console.log(data);
+
+            for (var i = 0; i < data.length; i++) {
+                var artworkDiv = $("<div>");
+                artworkDiv.addClass("col-12");
+                var deleteBtn = $("<button>");
+                artworkDiv.append("<h4>" + data[i].title + "</h4>");
+                artworkDiv.append(deleteBtn)
+                deleteBtn.addClass("deleteArtwork btn btn-dark");
+                deleteBtn.text("DELETE");
+                deleteBtn.attr("data-id", data[i].id);
+                $("#artworkDelete").append(artworkDiv);
+            }
+
+
+        })
+    }
+    getArtworks();
+
+    $(document).on("click", ".deleteArtwork", function () {
+
+        var artworkID = $(this).attr("data-id");
+
+        $.ajax({
+            method: "DELETE",
+            url: "/api/artwork/" + artworkID
+        }).then(getArtworks());
+    })
+
 })
+
